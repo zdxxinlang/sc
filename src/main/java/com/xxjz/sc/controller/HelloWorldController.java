05da074c53b4cf377a64e7ce89877ddd06d5e8c7
zdxxinlang <zdxxinlang@sina.cn>
2020-07-25 23:08:56 +0800

    first commit

---
+++ src/main/java/com/xxjz/sc/controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.xxjz.sc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorldController {
+
+    @ResponseBody
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello world";
+    }
+}
